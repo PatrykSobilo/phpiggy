d07d40dcba3682d448d8ee135b5fa028aa63327b
PatrykSobilo <127145148+PatrykSobilo@users.noreply.github.com>
2025-02-04 21:26:22 +0100

    135 skonczone
    135 skonczone

--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -5,12 +5,11 @@ declare(strict_types=1);
 require __DIR__ . "/../../vendor/autoload.php";
 
 use Framework\App;
+use App\Controlers\HomeController;
 
 $app = new App();
 
-$app->get('/', ['App\Controllers\HomeController', 'home']);
-
-dd($app);
+$app->get('/', [HomeController::class, 'home']);
 
 return $app;
 ?>
--- src/Framework/Router.php
+++ src/Framework/Router.php
@@ -27,6 +27,16 @@ class Router {
     return $path;
   }
 
+  public function dispatch(string $path, string $method) {
+    $path = $this->normalizePath($path);
+    $method = strtoupper($method);
+
+    echo $path.$method;
+  }
+
+
+
+
 }
 
 ?>
