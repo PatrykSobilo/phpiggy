55192b20a97c0ca5b4ae353ff163305083b7bed9
PatrykSobilo <127145148+PatrykSobilo@users.noreply.github.com>
2025-02-24 19:16:08 +0100

    149 begin
    149 begin

--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -5,11 +5,12 @@ declare(strict_types=1);
 require __DIR__ . "/../../vendor/autoload.php";
 
 use Framework\App;
-use App\Controlers\HomeController;
+use App\Controlers\{HomeController, AboutController};
 
 $app = new App();
 
 $app->get('/', [HomeController::class, 'home']);
+$app->get('/about', [AboutController::class, 'about']);
 
 return $app;
 ?>
