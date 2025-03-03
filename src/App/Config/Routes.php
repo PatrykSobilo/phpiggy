14ffa7adc4ae5d42aade57e54f039fb5551be1cc
PatrykSobilo <127145148+PatrykSobilo@users.noreply.github.com>
2025-03-03 18:20:21 +0100

    174 done
    174 done

--- src/App/Config/Routes.php
+++ src/App/Config/Routes.php
@@ -5,9 +5,10 @@ declare(strict_types=1);
 namespace App\Config;
 
 use Framework\App;
-use App\Controlers\{HomeController, AboutController};
+use App\Controlers\{HomeController, AboutController, AuthController};
 
 function registerRoutes(App $app) {
     $app->get('/', [HomeController::class, 'home']);
     $app->get('/about', [AboutController::class, 'about']);
+    $app->get('register', [AuthController::class, 'registerView']);
 }
