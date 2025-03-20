6fe48f1a6bcd8b3f7d6c81748821935825b7cb55
PatrykSobilo <127145148+PatrykSobilo@users.noreply.github.com>
2025-03-20 20:46:24 +0100

    217 done
    217 done

---
+++ cli.php
@@ -0,0 +1,22 @@
+<?php
+
+$driver = 'mysql';
+$config = http_build_query(data: [
+    'host' => 'localhost',
+    'port' => 3306,
+    'dbname' => 'phpiggy',
+], arg_separator: ';');
+
+$dsn = "{$driver}:{$config}";
+$username = 'root';
+$password = '';
+
+try{
+$db = new PDO($dsn, $username, $password);
+} catch(PDOException $e) {
+    die("Unable to connect to database");
+}
+
+
+
+echo "connected";
