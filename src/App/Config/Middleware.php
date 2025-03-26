47a59591ba1b9694fc070441276533c9d30d1fbf
PatrykSobilo <127145148+PatrykSobilo@users.noreply.github.com>
2025-03-26 19:28:31 +0100

    254 done
    254 done

--- src/App/Config/Middleware.php
+++ src/App/Config/Middleware.php
@@ -5,9 +5,10 @@ declare(strict_types=1);
 namespace App\Config;
 
 use Framework\App;
-use App\Middleware\{TemplateDataMiddleware, ValidationExceptionMiddleware, SessionMiddleware, FlashMiddleware};
+use App\Middleware\{TemplateDataMiddleware, ValidationExceptionMiddleware, SessionMiddleware, FlashMiddleware, CsrfTokenMiddleware};
 
 function registerMiddleware(App $app) {
+    $app->addMiddleware(CsrfTokenMiddleware::class);
     $app->addMiddleware(TemplateDataMiddleware::class);
     $app->addMiddleware(ValidationExceptionMiddleware::class);
     $app->addMiddleware(FlashMiddleware::class);
