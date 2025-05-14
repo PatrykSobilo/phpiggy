b1fd442bf13824321dc5cd73cc9b2d0784cb0dfb
PatrykSobilo <127145148+PatrykSobilo@users.noreply.github.com>
2025-05-14 19:00:57 +0200

    done
    done

---
+++ src/App/views/partials/bullshit.php
@@ -0,0 +1,16 @@
+<section id="header">
+  <div class="container">
+    <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3">
+
+      <ul class="nav col-12 col-md-auto mb-2 mb-md-0">
+        <li><a href="index.html" class="nav-link px-2 link-secondary">Home</a></li>
+        <li><a href="about.php" class="nav-link px-2">About</a></li>
+      </ul>
+
+      <div class="col-md-3 text-end">
+        <button type="button" class="btn btn-outline-primary me-2"><a href="loginForm.php">Login</a></button>
+        <button type="button" class="btn btn-outline-primary"><a href="registerForm.php">Register</a></button>
+      </div>
+    </header>
+  </div>
+</section>
